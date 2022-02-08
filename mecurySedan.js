8a1ca021d783fb33bd7df48c9930d933062b1c0d
Kaleab007 <abatenehkaleab@gmail.com>
2022-02-07 20:10:55 -0800

    SyntaxError: unterminated string literal

--- mecurySedan.js
+++ mecurySedan.js
@@ -1,6 +1,50 @@
-//this includes the vehicle class as a module
-const VehicleModule = require("./vehicle")
+import Vehicle from "./vehicleBaseClass.js"
 
-//this shows how to call from this module...
-let v = new VehicleModule.Vehicle("Mecury", "Sedan", "1965", "color", "mileage");
-console.log(v.make)
+
+//console logs 
+console.log ('${mdx.model} ${mdx.make} ${mdx.scheduleCarService(mdx.milage)}')
+console.log ('${Rav4.model} ${Rav4.make}${Rav4.scheduleCarService(Rav4.mileage)}')
+
+//classes section\\ 
+class Car extends Vehicle {
+    constructor (model,make,year,color,mileage)
+    { super (model,make,year,color,mileage);
+this.passenger=0;
+this.numberOfWheels= 4;
+this.maxPassengers=7;
+this.fuel=10;
+this.maxSpeed=160
+this.scheduleService = false;
+    }
+
+}
+//seating chart\\
+loadPassenger(num) 
+    if (this.passenger <this.maxPassengers) {
+        if((num + this.passenger) < this.maxPassengers) 
+           this.passenger = num;
+            return `can fit ${this.passenger} more! Extra room = ${Extraroom = true};
+
+    }
+      else {
+            
+            Extraroom == false;
+            return 'can Not fit ${this.passenger} more! Extra room = ${Extraroom = false};
+        }
+
+
+scheduleCarService(mileage) {
+    if (mileage > 25000) {
+        return 'current mileage is ${mileage}. Maintenance required = ${this.scheduleService = true}';
+    }
+else { 
+    return 'current mileage is ${mileage}. Maintenance required = ${this.scheduleService = false}';
+}
+    
+let Rav4 = new Car("Toyota", "Rav4", "2021", "two-tone", "0");
+console.log('${Rav4.model} ${Rav4.make} ${Rav4.loadPassenger (5)}');
+
+let mdx = new Car ("Acura", "mdx", "2017", "grey", "89141")
+console.log('${mdx.model} ${mdx.make} ${mdx.loadPassenger (7)}');
+
+console.log ('Can ${Rav4.model} start?  ${Rav4.start()}
